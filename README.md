d63b6bdb7a56b80cb0c0019271ed80b0b2a06a64
Olivier_Luethy <olivier.luethy@gmx.net>
2024-01-11 19:13:03 +0100

    added icon

--- README.md
+++ README.md
@@ -1,36 +1,50 @@
 # IntelliGrade
-An app that can be used to calculate a lot of important things with grades!
+
+An app that calculates a lot of important things with grades!
+
 ## Overview
-Welcome to AcademicSmartHub! This application is designed to assist students in managing their academic progress and enhancing their overall performance in school.
+
+<p align="center">
+  <a href="https://github.com/olivierluethy/IntelliGrade">
+    <img src="images/favicon.ico" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Welcome to IntelliGrade!</h3>
+</p>
 
 ## Features
-- **Grade Calculation:** Easily calculate and analyze grades for a more informed understanding of academic progress.
-- **Performance Insights:** Gain insights into areas of improvement and receive recommendations for enhancing grades.
-- **Goal Setting:** Set academic goals and track progress towards achieving them for a successful academic journey.
-- **User-Friendly Interface:** Intuitive design for a seamless user experience, making it accessible for students of all levels.
+
+- **Grade Calculation:** Easily calculate your grade based on points.
+- **Calculate the grade you need:** You have a bad average and want to improve it? Calculate the next grade you need!
+- **Grade increase or decrease:** Often the teacher raises the grades. You can use this function to calculate the raise.
 
 ## Getting Started
+
 1. Clone this repository to your local machine.
 2. Open the application in your preferred web browser.
 3. Start utilizing the features to enhance your academic experience.
 
 ## Dependencies
+
 - This application is built using [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript), [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML), and [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS).
 
 ## How to Contribute
+
 We welcome contributions to make AcademicSmartHub even better! Feel free to fork the repository, make your improvements, and submit a pull request.
 
 ## License
+
 This project is licensed under the [MIT License](LICENSE).
 
 ## Contact
+
 If you have any questions or feedback, please contact us at [spamemailadresseol221@gmail.com].
 
 Enhance your academic journey with IntelliGrade - Your Smart Academic Companion!
 
 ## Usefull links
-https://stackoverflow.com/questions/51578629/how-can-i-put-an-input-in-the-alert-box
-https://stackoverflow.com/questions/3330193/early-exit-from-function
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN?retiredLocale=de
 
-https://www.google.com/search?q=javascript+convert+to+float&oq=javascript+convert+to+float&aqs=chrome..69i57.9452j0j1&sourceid=chrome&ie=UTF-8
+- https://stackoverflow.com/questions/51578629/how-can-i-put-an-input-in-the-alert-box <br>
+- https://stackoverflow.com/questions/3330193/early-exit-from-function <br>
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN?retiredLocale=de <br>
+- https://www.google.com/search?q=javascript+convert+to+float&oq=javascript+convert+to+float&aqs=chrome..69i57.9452j0j1&sourceid=chrome&ie=UTF-8 <br>
--- script.js
+++ script.js
@@ -1,93 +1,101 @@
 function calculateGrade() {
-    const earnedPoints = parseInt(document.getElementById("earnedPoints").value);
-    const maxPoints = parseInt(document.getElementById("maxPoints").value);
+  const earnedPoints = parseInt(document.getElementById("earnedPoints").value);
+  const maxPoints = parseInt(document.getElementById("maxPoints").value);
 
-    if (!isNaN(earnedPoints) && !isNaN(maxPoints) && maxPoints !== 0) {
-        const grade = (earnedPoints * 5) / maxPoints + 1;
-        document.getElementById("showGrade").innerHTML = `Your grade is: ${grade.toFixed(2)}`;
-    } else {
-        console.error("Invalid input. Please enter valid numeric values.");
-    }
+  if (!isNaN(earnedPoints) && !isNaN(maxPoints) && maxPoints !== 0) {
+    const grade = (earnedPoints * 5) / maxPoints + 1;
+    document.getElementById(
+      "showGrade"
+    ).innerHTML = `Your grade is: ${grade.toFixed(2)}`;
+  } else {
+    console.error("Invalid input. Please enter valid numeric values.");
+  }
 }
 
 function calculateMagicMark(totalMarks, numMarks) {
-    const desiredAverage = prompt("Type your desired average");
+  const desiredAverage = prompt("Type your desired average");
 
-    if (desiredAverage === null) {
-        return; // User pressed cancel or closed the prompt
+  if (desiredAverage === null) {
+    return; // User pressed cancel or closed the prompt
+  }
+
+  const magicMark = (numMarks + 1) * desiredAverage - totalMarks;
+
+  if (magicMark > 6) {
+    alert("The desired mark isn't possible");
+  } else {
+    alert(`You need a ${magicMark.toFixed(2)}`);
+    const shouldCalculateAgain = confirm("Calculate again or stop?");
+
+    if (shouldCalculateAgain) {
+      calculateAverageMarks();
     }
-
-    const magicMark = (numMarks + 1) * desiredAverage - totalMarks;
-
-    if (magicMark > 6) {
-        alert("The desired mark isn't possible");
-    } else {
-        alert(`You need a ${magicMark.toFixed(2)}`);
-        const shouldCalculateAgain = confirm("Calculate again or stop?");
-
-        if (shouldCalculateAgain) {
-            calculateAverageMarks();
-        }
-    }
+  }
 }
 
 // Prompts the user to enter marks until they stop the input
 function calculateAverageMarks() {
-    let total_marks = 0;
-    let num_marks = 0;
+  let total_marks = 0;
+  let num_marks = 0;
 
-    // Loops until the user inputs 's' to stop
-    while (true) {
-        let mark = prompt("Type a mark (s to stop)");
-        if (mark === "s") {
-            calculateMagicMark(total_marks, num_marks);
-            break;
-        } else if (mark === null) {
-            break;
-        } else {
-            mark = parseFloat(mark);
-            if (isNaN(mark)) {
-                alert("Invalid input. Please enter a valid mark or 's' to stop.");
-            } else if (mark > 6) {
-                alert("Mark isn't valid");
-            } else {
-                total_marks += mark;
-                num_marks += 1;
-            }
-        }
-    }
-    if (num_marks > 0) {
-        let average = total_marks / num_marks;
-        alert("The average is " + average);
+  // Loops until the user inputs 's' to stop
+  while (true) {
+    let mark = prompt("Type a mark ('s' to stop)");
+    if (mark === "s") {
+      calculateMagicMark(total_marks, num_marks);
+      break;
+    } else if (mark === null) {
+      break;
     } else {
-        alert("No marks were entered");
+      mark = parseFloat(mark);
+      if (isNaN(mark)) {
+        alert("Invalid input. Please enter a valid mark or 's' to stop.");
+      } else if (mark > 6) {
+        alert("Mark isn't valid");
+      } else {
+        total_marks += mark;
+        num_marks += 1;
+      }
     }
+  }
+  if (num_marks > 0) {
+    let average = total_marks / num_marks;
+    alert("The average is " + average);
+  } else {
+    alert("No marks were entered");
+  }
 }
 
 // This function calculates the increase or decrease in percentage of the raised grade compared to the original grade
 function calculateGradeInDecrease() {
-    // Get the original grade from the input field with the id "original_grade"
-    const originalGrade = parseFloat(document.getElementById("original_grade").value);
+  // Get the original grade from the input field with the id "original_grade"
+  const originalGrade = parseFloat(
+    document.getElementById("original_grade").value
+  );
 
-    // Get the raised grade from the input field with the id "raised_grade"
-    const raisedGrade = parseFloat(document.getElementById("raised_grade").value);
+  // Get the raised grade from the input field with the id "raised_grade"
+  const raisedGrade = parseFloat(document.getElementById("raised_grade").value);
 
-    // Calculate the increase by subtracting the original grade from the raised grade
-    let increase = raisedGrade - originalGrade;
+  // Calculate the increase by subtracting the original grade from the raised grade
+  let increase = raisedGrade - originalGrade;
 
-    // Calculate the percentage increase by dividing the increase by the original grade
-    let percentageIncrease = (increase / originalGrade) * 100;
+  // Calculate the percentage increase by dividing the increase by the original grade
+  let percentageIncrease = (increase / originalGrade) * 100;
 
-    // Check if the increase is greater than 0, less than 0, or equal to 0
-    if (increase > 0) {
-        // If the increase is greater than 0, display the percentage increase with the text "It got increased by: "
-        document.getElementById("showIncrease").innerHTML = `It got increased by: <span style="font-weight: bold;">${percentageIncrease}%</span>`;
-    } else if (increase < 0) {
-        // If the increase is less than 0, take the absolute value of the percentage increase and display it with the text "It got decreased by: "
-        percentageIncrease = Math.abs(percentageIncrease);
-        document.getElementById("showIncrease").innerHTML = `It got decreased by: <span style="font-weight: bold;">${percentageIncrease}%</span>`;
-    } else {
-        // If the increase is equal to 0, display the text "There was no change."
-        document.getElementById("showIncrease").innerHTML = `There was no change.`;
-    }
+  // Check if the increase is greater than 0, less than 0, or equal to 0
+  if (increase > 0) {
+    // If the increase is greater than 0, display the percentage increase with the text "It got increased by: "
+    document.getElementById(
+      "showIncrease"
+    ).innerHTML = `It got increased by: <span style="font-weight: bold;">${percentageIncrease}%</span>`;
+  } else if (increase < 0) {
+    // If the increase is less than 0, take the absolute value of the percentage increase and display it with the text "It got decreased by: "
+    percentageIncrease = Math.abs(percentageIncrease);
+    document.getElementById(
+      "showIncrease"
+    ).innerHTML = `It got decreased by: <span style="font-weight: bold;">${percentageIncrease}%</span>`;
+  } else {
+    // If the increase is equal to 0, display the text "There was no change."
+    document.getElementById("showIncrease").innerHTML = `There was no change.`;
+  }
 }
